740386e6c627742bb76dc102a96f680bd9c1b77c
jgcordobaloyola <jgcordobaloyola@gmail.com>
2018-06-23 10:51:52 -0400

    Tercera Parte

---
+++ database/migrations/2018_06_23_143212_create_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('genero',1);
+            $table->char('Situacion_Sintimental',20);
+            $table->date('fecha_nacimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+}
